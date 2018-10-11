fa54e46d36694064b94f0be14ac7a71808bda48f
Jeremy Voros <jeremy@Jeremys-MacBook-Pro.local>
2018-10-11 13:36:53 -0600

    basic setup

--- App.js
+++ App.js
@@ -1,7 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text, Button } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="blue"
+        />
+        <Text>HomeStack</Text>
+        <Button
+          title="Go to Card Screen"
+          onPress={() => this.props.navigation.navigate('Card')}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+class CardScreen extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>CardScreen</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Card: CardScreen
+});
+
+class CardsStack extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>CardsStack</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+class CategoriesStack extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>CategoriesStack</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+class FavoritesStack extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>FavoritesStack</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+class AboutStack extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>AboutStack</Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default createBottomTabNavigator({
+  Latest: HomeStack,
+  Cards: CardsStack,
+  Categories: CategoriesStack,
+  Favs: FavoritesStack,
+  About: AboutStack
+});
+
+// Demo code
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
