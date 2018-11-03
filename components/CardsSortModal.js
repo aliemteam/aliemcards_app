7a032613e863a2ec04d9c1047206d110ca1ae714
Jeremy Voros <jeremy@Jeremys-MacBook-Pro.local>
2018-11-03 02:08:34 -0600

    fix cancel button

--- components/CardsSortModal.js
+++ components/CardsSortModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-    Button,
     Modal,
     Text,
     SafeAreaView,
@@ -8,17 +7,26 @@ import {
     TouchableOpacity,
     View
 } from 'react-native';
+import { Ionicons as Icon } from '@expo/vector-icons';
 
 import { getCategories } from './CardLibrary';
 import { analyzeThis } from './utils';
 import Colors from './colors';
 
 const ListItem = (props) =>
-    <TouchableOpacity onPress={() => {props.onPress()}}>
+    <TouchableOpacity onPress={props.onPress}>
         <View style={{borderBottomWidth: 1, borderColor: Colors.lightGray }}>
             <Text style={{ fontFamily: 'open-sans-regular', fontSize: 16, padding: 10 }}>
                 {props.cat.name}
             </Text>
+        </View>
+    </TouchableOpacity>
+
+const ButtonIcon = (props) => 
+    <TouchableOpacity onPress={props.onPress} style={{ height: 30 }}>
+        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <Icon name='md-close' size={25} style={{ color: Colors.primary}} />
+            <Text style={{ paddingLeft: 5, paddingBottom: 5, fontSize: 18, color: Colors.primary }}>close</Text>
         </View>
     </TouchableOpacity>
 
@@ -50,8 +58,8 @@ class CardsSortModal extends React.Component {
                         padding: 20,
                         backgroundColor: 'rgba(0,176,146,.8)',
                     }}>
-                    <View style={{ flex: 1, margin: 30, padding: 20, backgroundColor: 'white', overflow: 'hidden'}}>
-                        <Button  title="Cancel" onPress={() => { this.props.closeModal()} }  />
+                    <View style={{ flex: 1, margin: 30, padding: 20, backgroundColor: 'white'}}>
+                        <ButtonIcon onPress={() => {this.props.closeModal()} } />
                         <ScrollView>
                             <ListItem key={99999999999} cat={{ name: 'All Categories' }} onPress={() => this.props.onPress(null)} />
                             { this.state.cats.map((cat,index) => 
